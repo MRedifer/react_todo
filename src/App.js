6343fa0961596cb831d6a9a23b9a1993b30b332f
Michael Redifer <mikeredifer@yahoo.com>
2022-04-06 10:00:16 -0500

    Added AuthProvider and ProtectedRoute

--- src/App.js
+++ src/App.js
@@ -1,40 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import Navigation from './componentes/Navigation';
+import Navigation from './components/Navigation';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
-import Login from './componentes/Login/Login';
-import Todos from './componentes/Todos/Todos';
-import Categories from './componentes/Categories/Categories';
-import NotFound from './componentes/NotFound';
+import Login from './components/Auth/Login';
+import Todos from './components/Todos/Todos';
+import Categories from './components/Categories/Categories';
+import NotFound from './components/NotFound';
+import AuthProvider from './contexts/AuthContext';
+import ToDo from './components/ToDo/ToDo';
+import ProtectedRoute from './ProtectedRoute';
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
       <Router>
       <Navigation />
       <Routes>
-        {/* <Route path='/' element={< />} /> */}
+        <Route path='/' element={<ToDo />} />
         <Route path='/login' element={<Login />} />
-        <Route path='/todos' element={<Todos />} />
+        <Route path='/todos' element={<ProtectedRoute><Todos /></ProtectedRoute>} />
         <Route path='/categories' element={<Categories />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
       </Router>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </AuthProvider>
     </div>
   );
 }
